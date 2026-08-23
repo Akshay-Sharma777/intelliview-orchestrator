922ce181cb9770ca0d32cb312f654747aca9f6d6
Akshay Sharma <akshshar322201@gmail.com>
2026-08-23 08:07:39 +0530

    fix: merge alembic migration heads

---
+++ alembic/versions/850f7086ffdd_merge_heads.py
@@ -0,0 +1,28 @@
+"""merge heads
+
+Revision ID: 850f7086ffdd
+Revises: 003_add_candidate_deleted_at, 25b9705eb8d5
+Create Date: 2026-08-23 08:07:05.596843
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '850f7086ffdd'
+down_revision: Union[str, Sequence[str], None] = ('003_add_candidate_deleted_at', '25b9705eb8d5')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
