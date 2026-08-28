f7dfa2e2c84ace7d50d30e6d826cc2cb5a76470b
ankit-269 <ankit00000gg@gmail.com>
2026-08-28 21:47:24 +0530

    style: sort migration imports

--- alembic/versions/aa56afa4b0bc_add_version_to_interview_templates.py
+++ alembic/versions/aa56afa4b0bc_add_version_to_interview_templates.py
@@ -8,8 +8,9 @@ Create Date: 2026-08-28 19:30:43.341760
 
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
+
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "aa56afa4b0bc"
