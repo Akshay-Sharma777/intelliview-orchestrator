db36d21349b5362412e70581debf51cc9fb30fc9
ankit-269 <ankit00000gg@gmail.com>
2026-08-28 23:16:18 +0530

    fix: update migration chain for stabilized version

--- alembic/versions/aa56afa4b0bc_add_version_to_interview_templates.py
+++ alembic/versions/aa56afa4b0bc_add_version_to_interview_templates.py
@@ -14,7 +14,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "aa56afa4b0bc"
-down_revision: Union[str, Sequence[str], None] = "002_add_llm_usage"
+down_revision: Union[str, Sequence[str], None] = "850f7086ffdd"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
