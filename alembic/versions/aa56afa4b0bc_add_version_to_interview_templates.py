e6458c29612f5c87af554c2c5faa3520608e12a3
ankit-269 <ankit00000gg@gmail.com>
2026-08-28 21:33:17 +0530

    style: format interview template migration

--- alembic/versions/aa56afa4b0bc_add_version_to_interview_templates.py
+++ alembic/versions/aa56afa4b0bc_add_version_to_interview_templates.py
@@ -11,7 +11,6 @@ from typing import Sequence, Union
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
 revision: str = "aa56afa4b0bc"
 down_revision: Union[str, Sequence[str], None] = "002_add_llm_usage"
