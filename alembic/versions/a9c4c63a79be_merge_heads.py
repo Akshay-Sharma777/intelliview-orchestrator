32255c5f5ac513f01c1729158a51c7e09840d6e1
Sanjana-Chatlapalli <sanjanachatlapalli@gmail.com>
2026-08-29 21:21:16 +0530

    Fix alembic merge head reference to point at renamed revision b08fe3635da3

--- alembic/versions/a9c4c63a79be_merge_heads.py
+++ alembic/versions/a9c4c63a79be_merge_heads.py
@@ -1,7 +1,7 @@
 """merge heads
 
 Revision ID: a9c4c63a79be
-Revises: 003_add_candidate_features, 850f7086ffdd
+Revises: 003_add_candidate_features, b08fe3635da3
 Create Date: 2026-08-28 22:35:13.855443
 
 """
@@ -12,7 +12,7 @@ from collections.abc import Sequence
 revision: str = "a9c4c63a79be"
 down_revision: str | Sequence[str] | None = (
     "003_add_candidate_features",
-    "850f7086ffdd",
+    "b08fe3635da3",
 )
 branch_labels: str | Sequence[str] | None = None
 depends_on: str | Sequence[str] | None = None
